b62a2f373430584cc5f127aaeb01322e87c774c7
AidAliu <aidaliu50@gmail.com>
2024-07-27 17:21:48 +0200

    Initial commit

---
+++ src/main/java/com/company/CuriousHub/CuriousHubApplication.java
@@ -0,0 +1,26 @@
+package com.company.CuriousHub;
+
+
+import com.company.CuriousHub.user.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class CuriousHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuriousHubApplication.class, args);
+
+
+	}
+	@GetMapping
+	public List<User> hello() {
+		return List.of(new User(1L, "AlbertEinstein","relativity299792458","einstein@gmail.com"));
+	}
+
+}
